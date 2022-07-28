cdf6a44dfde6633023768c6c248e6eae8af48187
Sankha Subhra Mukherjee <sankha.mukherjee@danone.com>
2022-07-29 00:54:27 +0800

    a very simple library has been created

--- main.py
+++ main.py
@@ -1,38 +1,14 @@
 import json, os
-import pymysql, pymysql.cursors
-import sshtunnel
+from utils.dbIO import mysqlIO
 
 config   = json.load( open('config/config.json') )
-dbConfig = json.load( open('config/dbConfig.json') )
 
-def main():
+def testData():
 
-    TUNNEL = dbConfig['sshTunnel']
-    MUM    = dbConfig['mumDB'] 
+    results = mysqlIO.getAllData('select * from BOTTLE_FEEDING limit 10;')
+    print(results)
 
-    with sshtunnel.SSHTunnelForwarder(
-        ( TUNNEL['host'], 22),
-        ssh_username        = TUNNEL['user'],
-        ssh_pkey            = TUNNEL['pem_file'],
-        remote_bind_address = (MUM['host'], MUM['port']),
-        local_bind_address  = ('127.0.0.1', MUM['port']),
-    ) as tunnel:
-        try:
-            
-            connection = pymysql.connect(
-                host     = '127.0.0.1',
-                port     = MUM['port'],
-                user     = MUM['user'],
-                password = MUM['password'],
-                database = MUM['database'],
-            )
-            with connection.cursor() as cursor:
-                cursor.execute('select * from BOTTLE_FEEDING limit 10;')
-                result = cursor.fetchall()
-                print( result )
-        finally:
-            connection.close()
-
+    return
 
 if __name__ == '__main__':
-    main()
+    print(testData())
---
+++ utils/dbIO/mysqlIO.py
@@ -0,0 +1,42 @@
+import sshtunnel
+import pymysql, pymysql.cursors
+import json
+
+dbConfig = json.load( open('config/dbConfig.json') )
+
+def getAllData(query, params=None):
+
+    result = None
+
+    TUNNEL = dbConfig['sshTunnel']
+    MUM    = dbConfig['mumDB'] 
+
+    with sshtunnel.SSHTunnelForwarder(
+        ( TUNNEL['host'], 22),
+        ssh_username        = TUNNEL['user'],
+        ssh_pkey            = TUNNEL['pem_file'],
+        remote_bind_address = (MUM['host'], MUM['port']),
+        local_bind_address  = ('127.0.0.1', MUM['port']),
+    ) as tunnel:
+        try:
+            
+            connection = pymysql.connect(
+                host     = '127.0.0.1',
+                port     = MUM['port'],
+                user     = MUM['user'],
+                password = MUM['password'],
+                database = MUM['database'],
+            )
+            with connection.cursor() as cursor:
+                if params is None:
+                    cursor.execute(query)
+                else:
+                    cursor.execute(query, params)
+                result = cursor.fetchall()
+                print( result )
+        finally:
+            connection.close()
+
+
+
+    return result
